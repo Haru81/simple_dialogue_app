fd954460131fdb7c2bed705b1852e8b0a07039c3
Haru81 <h.azowake1@gmail.com>
2025-05-25 15:51:10 +0900

    [Fix]プロンプトの修正

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-colab_url = "https://1e82-35-230-163-92.ngrok-free.app/process"
+colab_url = "https://fea6-34-83-131-80.ngrok-free.app/process"
 
 st.title("対話アプリ")
 st.text("会話内容を入れてください。")
@@ -9,17 +9,16 @@ st.text("会話内容を入れてください。")
 # 会話内容入力フォーム
 text_input = st.text_input('Input')
 
-st.write(f"Debug: text_input = '{text_input}'")
-
 if text_input.strip():
     data = {"text": text_input}
     try:
         response = requests.post(colab_url, json=data)
-        print(response.status_code)
-        print(data)
+        print(f"status code: {response.status_code}\ndata: {data}")
         if response.status_code == 200:
             result = response.json()
             st.write(f"モデル: {result['response']}")
+        elif response.status_code == 404:
+            st.error("Colabサーバーが起動していないか接続できません")
         else:
             st.error("Colab サーバーでエラーが発生しました！")
     except requests.exceptions.RequestException as e:
